1de97f8b82201f3bb871ea7d87e8cbbd33503aa3
Paweł Winkler <147107563+PawWin@users.noreply.github.com>
2024-04-08 20:42:48 +0200

    getting data from api and displaying it in html

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template,request
+import json
+import requests
 
-app = Flask(__name__)
+app = Flask(__name__,  template_folder='./templates')
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+@app.route('/posts')
+def posts():
+    r_p = requests.get('https://jsonplaceholder.typicode.com/posts')
+    r_k = requests.get('https://jsonplaceholder.typicode.com/comments')
+    data_posts = r_p.json()
+    data_kom = r_k.json()
+    return render_template('posts.html',data_posts = data_posts,data_kom = data_kom, get_comments=get_comments)
 
-
+def get_comments(post_id):
+    r = requests.get(f'https://jsonplaceholder.typicode.com/posts/{post_id}/comments')
+    return len(r.json())
 if __name__ == '__main__':
     app.run()
