50b95736a3b445b04f523a51999b14ebe1d4fbe0
Paweł Winkler <147107563+PawWin@users.noreply.github.com>
2024-04-10 20:44:27 +0200

    created all subpages

--- app.py
+++ app.py
@@ -10,13 +10,29 @@ def index():
 @app.route('/posts')
 def posts():
     r_p = requests.get('https://jsonplaceholder.typicode.com/posts')
-    r_k = requests.get('https://jsonplaceholder.typicode.com/comments')
     data_posts = r_p.json()
-    data_kom = r_k.json()
-    return render_template('posts.html',data_posts = data_posts,data_kom = data_kom, get_comments=get_comments)
+    return render_template('posts.html',data_posts = data_posts)
+
+@app.route('/comments')
+def comments():
+    r_c = requests.get('https://jsonplaceholder.typicode.com/comments')
+    data_comments = r_c.json()
+    return render_template('comments.html',data_comments = data_comments)
+@app.route('/photos')
+def photos():
+    r_p = requests.get('https://jsonplaceholder.typicode.com/photos')
+    data_photos = r_p.json()
+    return render_template('photos.html',data_photos = data_photos)
+@app.route('/todos')
+def albums():
+    r_a = requests.get('https://jsonplaceholder.typicode.com/albums')
+    data_albums = r_a.json()
+    return render_template('albums.html',data_albums = data_albums)
+
 
 def get_comments(post_id):
     r = requests.get(f'https://jsonplaceholder.typicode.com/posts/{post_id}/comments')
     return len(r.json())
+
 if __name__ == '__main__':
     app.run()
